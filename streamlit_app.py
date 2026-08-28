af4750fc1b14ac2f8fe3c499631a618e91bafee9
laliparth <laliparth@gmail.com>
2026-08-28 21:21:04 +0530

    Implement user login and access control
    Added user authentication and authorization features, including a login screen and allowed users list.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,38 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+ALLOWED_USERS = {
+    "laliparth@gmail.com",
+    "zaiddyd42@gmail.com",
+    "shreyagoyal1733@gmail.com",
+    "dharneet08@gmail.com",
+    "shounak6425@gmail.com"
+    "guptatanush763@gmail.com"
+}
 
-st.write('Hello world!')
+
+def login_screen():
+    st.title("Intelligent Border Video Analytics Platform")
+    st.subheader("Secure Surveillance Portal")
+
+    st.button("Login with Google", on_click=st.login)
+
+
+if not st.user.is_logged_in:
+    login_screen()
+    st.stop()
+
+
+if st.user.email not in ALLOWED_USERS:
+    st.error("You are not authorized to access this system.")
+    st.button("Logout", on_click=st.logout)
+    st.stop()
+
+
+st.title("Intelligent Border Video Analytics Platform")
+
+st.success(f"Welcome, {st.user.name}!")
+
+st.write("We will add our app here...")
+
+if st.button("Logout"):
+    st.logout()
